107fca6ee8144afdd5ca5e3527d6d62644145218
Adriano Caloiaro <me@adriano.fyi>
2023-06-20 16:26:48 -0600

    Fix leaky stream on channel switch bug

--- player/player.go
+++ player/player.go
@@ -45,6 +45,7 @@ func Play(ctx *context.AppContext, stream io.ReadWriter, playbackLatency int) (e
 func Stop(ctx *context.AppContext) {
 	ctx.IsPlaying = false
 	if ctx.Player != nil {
+		ctx.AudioStream.Close()
 		ctx.Player.Close()
 	}
 }

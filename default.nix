b6f3fd25f0e8d260972b457a2f87bc8905b8c59a
Adriano Caloiaro <code@adriano.fyi>
2024-02-25 10:22:46 -0700

    Update to version 1.8.0
    Signed-off-by: Adriano Caloiaro <code@adriano.fyi>

--- default.nix
+++ default.nix
@@ -1,20 +1,20 @@
-{ pkgs ? (
+{
+  pkgs ? (
     let
       inherit (builtins) fetchTree fromJSON readFile;
       inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
     in
-    import (fetchTree nixpkgs.locked) {
-      overlays = [
-        (import "${fetchTree gomod2nix.locked}/overlay.nix")
-      ];
-    }
-  )
-, buildGoApplication ? pkgs.buildGoApplication
+      import (fetchTree nixpkgs.locked) {
+        overlays = [
+          (import "${fetchTree gomod2nix.locked}/overlay.nix")
+        ];
+      }
+  ),
+  buildGoApplication ? pkgs.buildGoApplication,
 }:
-
 buildGoApplication {
   pname = "di-tui";
-  version = "v1.8.0";
+  version = "1.8.0";
   pwd = ./.;
   src = ./.;
   modules = ./gomod2nix.toml;
